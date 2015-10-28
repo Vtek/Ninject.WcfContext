abf151ce088f130e6ebd387197d341602c8e6ab8
unknown <sylvain.pontoreau@LAP-1580.valtech.com>
2015-10-28 18:13:40 +0100

    Base code

---
+++ src/Ninject.WcfContext/NinjectContextExtension.cs
@@ -0,0 +1,18 @@
+﻿using Ninject.CoreContext;
+
+namespace Ninject.WcfContext
+{
+    /// <summary>
+    /// Ninject Context extension
+    /// </summary>
+    public static class NinjectContextExtension
+    {
+        /// <summary>
+        /// Use WCF
+        /// </summary>
+        public static NinjectContext UseWcf(this NinjectContext context)
+        {
+            return context;
+        }
+    }
+}
